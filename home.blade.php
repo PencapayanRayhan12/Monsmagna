29e7a431c2e4c91331ed7b16bb2cca1b3db7bf9a
Tazakka22 <furrow.visages9z@icloud.com>
2025-05-20 22:41:03 +0700

    Add files via upload

---
+++ home.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mons Magna</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('style.css')}}">⁠
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+
+    <!-- header -->
+     <div class="medsos">
+        <div class="container">
+            <ul>
+                <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+                <a href="https://www.instagram.com/monsmagna/">
+                    
+                </a>
+            </ul>
+        </div>
+     </div>
+     <header>
+        <div class="container">
+        ⁠<h1><a href="/">MONS MAGNA</a></h1> 
+            <ul>
+                <li class="active"><a  href="/">HOME</a></li>
+                <li><a  href="/about">ABOUT</a></li>   
+                <li><a  href="/product">PRODUCT</a></li>   
+                <li><a  href="/tambahproduct">TAMBAH PRODUCT</a></li> 
+                <li><a  href="/blog">BLOG</a></li>     
+                <li><a  href="/contact">CONTACT</a></li>    
+            </ul>
+        </div>
+     </header>
+
+     <!-- banner -->
+      <section class="banner">
+         <h2></h2>
+      </section>
+
+      <!-- about -->
+       <section class="about">
+           <div class="container">
+                <h3></h3>
+                <p>“An exciting challenge to become a pioneer of handcraft classic leather shoes with traditional touch”</p>
+            </div>
+            </section>
+
+            <!-- service -->
+            <section class="services">
+                <div class="container">
+                    <h3>SERVICES</h3>
+                    <div class="box">
+                        <div class="col-4">
+                            <div class="icon"><i class="fa-solid fa-credit-card"></i></div>
+                            <h4>MEMBERSHIP</h4>
+                        </div>
+                        <div class="col-4">
+                            <div class="icon"><i class="fa-solid fa-cart-shopping"></i></i></div>
+                            <h4>CUSTOM ORDER</h4>
+                        </div>
+                        <div class="col-4">
+                            <div class="icon"><i class="fa-solid fa-truck"></i></i></div>
+                            <h4>FREE SHIPPING</h4>
+                        </div>
+                        <div class="col-4">
+                            <div class="icon"><i class="fa-solid fa-up-right-and-down-left-from-center"></i></i></div>
+                            <h4>SIZE GUIDE</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            <!-- footer -->
+             <footer>
+                <div class="container">
+                    <small> Copyright &copy; 2019 - Mons Magna, All Rights Reserved.</small>
+                </div>
+            </footer>
+        
+        
+                </div>
+             </section>
+             
+</body>   
+</html>
